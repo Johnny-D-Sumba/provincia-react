54b40cc956b6e243ab59194447459c9ee07fec6a
Johnny D. Sumba <jhonnydanilo177@gmail.com>
2022-07-24 23:44:56 -0500

    provinciaList created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 
+const baseUrl = 'http://localhost:9090/api/v1.0/provincia'
 function App() {
+  const [data, setData] = useState([]);
+
+  const peticionGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+      })
+  }
+
+  useEffect(async () => {
+    await peticionGet();
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer>
+        <Table variant='simple'>
+          <TableCaption>Imperial to metric conversion factors</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Nombre</Th>
+              <Th>Acciones</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map(provincia => (
+              <Tr key={provincia.id}>
+                <Td>{provincia.id}</Td>
+                <Td>{provincia.nombre}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
